1b8a804dbb1ee43b6f5294e8f0889702ef4eb802
perlancar (@pc-home) <perlancar@gmail.com>
2015-07-22 07:13:35 +0700

    Initial commit

---
+++ lib/App/lens.pm
@@ -0,0 +1,16 @@
+package App::chalk;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Colorize text for terminal output
+
+=head1 DESCRIPTION
+
+This distribution contains the script L<chalk>.
+
+
+=head1 SEE ALSO
+
+L<chalk>
